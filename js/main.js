85185fae07f0171db915ab20643c2369357f7cc1
BenSkooma <benjamin.launicke@googlemail.com>
2023-09-14 15:20:39 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const toggleMenuBtn = document.getElementsByClassName('mainnav-btn')[0]
 const mobileNav = document.getElementsByClassName('page-mainnav')[0]
+const pageBody = document.getElementsByClassName('page')[0]
 
 toggleMenuBtn.addEventListener('click', () => {
     mobileNav.classList.toggle('is-open')
+    pageBody.classList.toggle('noscroll')
 })
